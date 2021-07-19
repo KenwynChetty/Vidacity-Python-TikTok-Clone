76cb4e9c71e8a09ebb933f4c4b90dd3be3a51591
Kenwyn Chetty <kendyllgroup@gmail.com>
2021-07-19 17:41:47 +0200

    Vidacity

--- staticfiles/js/base_settings.js
+++ staticfiles/js/base_settings.js
@@ -18,11 +18,11 @@
             },
             success: function (response) {
               if (response.liked) {
-                $("#likebtn" + id).html('<svg xmlns="http://www.w3.org/2000/svg"  width="1.5em" height="1.5em" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16"  style="z-index:100; font-size:33px"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/> </svg>');
+                $("#likebtn" + id).html('<svg xmlns="http://www.w3.org/2000/svg"  width="1.5em" height="1.5em" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16"  style="z-index:100; font-size:25px"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/> </svg>');
                 $("#likebtn" + id).css("color", "red");
 
               } else {
-                $("#likebtn" + id).html('<svg xmlns="http://www.w3.org/2000/svg"  width="1.5em" height="1.5em" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16"  style="z-index:100; font-size:33px"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/> </svg>');
+                $("#likebtn" + id).html('<svg xmlns="http://www.w3.org/2000/svg"  width="1.5em" height="1.5em" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16"  style="z-index:100; font-size:25px"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/> </svg>');
                 $("#likebtn" + id).css("color", "white");
               }
             },
